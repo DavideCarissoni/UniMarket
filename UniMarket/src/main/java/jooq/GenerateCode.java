648572df52a32727c107ac4050218784d0e6707b
DavideCarissoni <d.carissoni@studenti.unibg.it>
2025-01-04 11:28:21 +0100

    Aggiunta jooq
    Aggiunta generazione di codice automatica con jooq e creazione di un nuovo branch

---
+++ UniMarket/src/main/java/jooq/GenerateCode.java
@@ -0,0 +1,27 @@
+package jooq;
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.*;
+
+public class GenerateCode {
+
+    public static void main(String[] args) {
+        try {
+            Configuration configuration = new Configuration()
+                .withJdbc(new Jdbc().withDriver("org.sqlite.JDBC")
+                .withUrl("jdbc:sqlite:src/main/db/database.db"))
+                .withGenerator(new Generator()
+                .withDatabase(new Database().withName("org.jooq.meta.sqlite.SQLiteDatabase") 
+                .withInputSchema("main"))
+                .withTarget(new Target().withPackageName("prodotti.jooq.generated") 
+                .withDirectory("src/generated/java")));
+
+            // Esecuzione della generazione del codice
+            GenerationTool.generate(configuration);
+            System.out.println("Codice generato con successo!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Errore durante la generazione del codice JOOQ.");
+        }
+    }
+}
